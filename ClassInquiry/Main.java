cf3bbf50f51b77769a3b232efd0a512c39f0eb83
sdvic <gvw@san.rr.com>
2014-09-04 21:12:33 -0700

    version 140904A

--- ClassInquiry/Main.java
+++ ClassInquiry/Main.java
@@ -1,6 +1,9 @@
+/*********************************************************
+ * Auto Response for Java Class Inquiries
+ * version 140904A by Vic
+ *********************************************************/
 import java.util.Date;
 import java.util.Properties;
-
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -9,9 +12,8 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class Main {
-
 	public static void main(String[] args) {
-	new Main().getGoing();
+		new Main().getGoing();
 	}
 
 	private void getGoing() {
@@ -21,30 +23,28 @@ public class Main {
 		String vic = "gvw@san.rr.com";
 		String diane = "dfrw@san.rr.com";
 		String june = "joonspoon@joonspoon.com";
-		 
+
 		Properties props = new Properties();
 		props.put("mail.smtp.host", host);
 		props.put("mail.debug", "true");
-		 
-		Session session = Session.getInstance(props);
-		 
-		try {
-		    Message msg = new MimeMessage(session);
-		    msg.setFrom(new InternetAddress(from));
-		 
-		    InternetAddress[] address = {new InternetAddress(vic), new InternetAddress(diane), new InternetAddress(june)};
-		    msg.setRecipients(Message.RecipientType.TO, address);
-		 
-		    msg.setSubject("Java Classes");
-		    msg.setSentDate(new Date());
-		 
-		    msg.setText("hello");
-		    Transport.send(msg);
-		}
-		catch (MessagingException mex) {
-		    mex.printStackTrace();
-		}
-		
-	}
 
+		Session session = Session.getInstance(props);
+
+		try {
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress(from));
+
+			InternetAddress[] address = { new InternetAddress(vic),
+					new InternetAddress(diane), new InternetAddress(june) };
+			msg.setRecipients(Message.RecipientType.TO, address);
+
+			msg.setSubject("Java Classes");
+			msg.setSentDate(new Date());
+
+			msg.setText("hello");
+			Transport.send(msg);
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
 }
